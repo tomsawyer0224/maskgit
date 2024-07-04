812a27f034786b875d3f477c1a0a47f21632396f
tomsawyer0224 <130035084+tomsawyer0224@users.noreply.github.com>
2024-07-04 08:45:47 +0700

    Add files via upload

---
+++ datasets.py
@@ -0,0 +1,148 @@
+import torch
+import lightning as L
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from PIL import Image
+import os
+
+class ToyDataset(Dataset):
+    def __init__(self, n_samples = 256, image_channel = 3, image_size = 32):
+        #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        torch.manual_seed(42)
+        self.n_samples = n_samples
+        self.images = torch.rand(n_samples,image_channel,image_size,image_size)
+        self.labels = torch.randint(low=0,high=4,size=(n_samples,))
+    def __len__(self):
+        return self.n_samples
+    def __getitem__(self, idx):
+        return self.images[idx], self.labels[idx]
+class ImageGenerationDataset(Dataset):
+    def __init__(self, root, transform = None):
+        '''
+        args:
+            root: path to image (str)
+            transform: torchvision transforms
+        rerturns: None
+        '''
+        dirs = os.listdir(root)
+        assert dirs, f'nothing found in {root}'
+        # check if root includes directories for files
+        if os.path.isdir(os.path.join(root,dirs[0])):
+            self.class_names = dirs
+            self.is_dir = True
+        else:
+            self.class_names = [root.split('/')[-1]]
+            self.is_dir = False
+        self.dirs = dirs # dirs are directories or files
+        self.root = root
+        self.transform = transform
+    def __len__(self):
+        pass
+    def __getitem__(self):
+        pass
+class LitImageGenerationDataset(L.LightningDataModule):
+    def __init__(
+        self, 
+        train_data: str|Dataset,
+        val_data: str|Dataset = None,
+        test_data: str|Dataset = None,
+        image_size: int = 256,
+        image_channel: int = 3,
+        batch_size: int = 16,
+    ) -> None:
+        '''
+        args:
+            train_data: path/to/train_data or torch Dataset
+            val_data: path/to/val_data or torch Dataset
+            test_data: path/to/test_data or torch Dataset
+            image_size: image size
+            image_channel: image channel
+            batch_size: batch size
+        '''
+        assert isinstance(train_data, (str, Dataset)), \
+        'train_data should be a str or a torch Dataset'
+        if val_data:
+            assert isinstance(val_data, (str, Dataset)), \
+            'val_data should be a str or a torch Dataset'
+        if test_data:
+            assert isinstance(test_data, (str, Dataset)), \
+            'test_data should be a str or a torch Dataset'
+        super().__init__()
+        self.image_size = image_size
+        self.image_channel = image_channel
+        self.batch_size = batch_size
+        train_transform = transforms.Compose([
+            transforms.Resize((self.image_size, self.image_size)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])
+        ])
+        val_transform = transforms.Compose([
+            transforms.Resize((self.image_size, self.image_size)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])
+        ])
+        if isinstance(train_data, str):
+            train_data = ImageFolder(
+                root = train_data,
+                transform = train_transform
+            )
+
+        if val_data:
+            if isinstance(val_data, str):
+                val_data = ImageFolder(
+                    root = val_data,
+                    transform = val_transform
+                )
+
+        if test_data:
+            if isinstance(test_data, str):
+                test_data = ImageFolder(
+                    root = test_data,
+                    transform = val_transform
+                )
+
+        if val_data and not test_data:
+            train_data, test_data = torch.utils.data.random_split(
+                dataset = train_data,
+                lengths = [0.8, 0.2],
+                generator = torch.Generator().manual_seed(42)
+            )
+        if not val_data and test_data:
+            train_data, val_data = torch.utils.data.random_split(
+                dataset = train_data,
+                lengths = [0.8, 0.2],
+                generator = torch.Generator().manual_seed(42)
+            )
+        if not val_data and not test_data:
+            train_data, val_data, test_data = torch.utils.data.random_split(
+                dataset = train_data,
+                lengths = [0.8, 0.1, 0.1],
+                generator = torch.Generator().manual_seed(42)
+            )
+        self.train_dataset = train_data
+        self.val_dataset = val_data
+        self.test_dataset = test_data
+    def train_dataloader(self):
+        return DataLoader(
+            dataset = self.train_dataset,
+            batch_size = self.batch_size,
+            shuffle = True,
+            num_workers = 2
+        )
+    def val_dataloader(self):
+        return DataLoader(
+            dataset = self.val_dataset,
+            batch_size = self.batch_size,
+            shuffle = False,
+            num_workers = 2
+        )
+    def test_dataloader(self):
+        return DataLoader(
+            dataset = self.test_dataset,
+            batch_size = self.batch_size,
+            shuffle = False,
+            num_workers = 2
+        )
---
+++ train.py
@@ -0,0 +1,279 @@
+import torch
+import yaml
+import argparse
+import os
+import lightning as L
+
+import datasets
+from utils import save_image, plot_metrics
+
+
+from models import (
+    VQGAN_Wrapper,
+    VQGAN,
+    Transformer_Wrapper,
+    Transformer
+)
+
+class TrainingVQGAN:
+    def __init__(self, config_file):
+        with open(config_file, 'r') as file:
+            configs = yaml.safe_load(file)
+        self.configs = configs
+    def run(self, max_epochs: int = None, ckpt_path: str = None, limit_batches: bool = False):
+        VQGAN_config = self.configs['VQGAN_config']
+        dataset_config = self.configs['dataset_config']
+        training_config = self.configs['training_config']
+        if max_epochs:
+            training_config['trainer_config']['max_epochs'] = max_epochs
+        if ckpt_path:
+            training_config['fit_config']['ckpt_path'] = ckpt_path
+
+        # VQGAN model
+        vqgan = VQGAN_config
+        #vqgan = VQGAN(**VQGAN_config)
+        
+        # dataset
+        datamodule = datasets.LitImageGenerationDataset(**dataset_config)
+
+        # Wrapper model
+        optimizer_config = training_config['optimizer_config']
+        wrapper_model = VQGAN_Wrapper(
+            model = vqgan,
+            **optimizer_config
+        )
+
+        # trainer config
+        trainer_config = training_config['trainer_config']
+        # ---logger
+        if trainer_config['logger']:
+            trainer_config['logger'] = L.pytorch.loggers.CSVLogger(
+                save_dir = trainer_config['default_root_dir'],
+                name = 'logs'
+            )
+
+        # for reproducibility: seed_everything, deterministic
+        L.seed_everything(42, workers=True)
+        if limit_batches: # for testing purpose
+            limit_batches = {
+                'limit_train_batches': 2,
+                'limit_test_batches': 2,
+                'limit_val_batches': 2
+            }
+        else:
+            limit_batches = {}
+        lr_monitor = L.pytorch.callbacks.LearningRateMonitor(
+            logging_interval = 'step'
+        )
+        trainer = L.Trainer(
+            **trainer_config,
+            deterministic=True,
+            enable_checkpointing = False, 
+            callbacks = [lr_monitor],
+            **limit_batches
+        )
+
+        # training phase
+        fit_config = training_config['fit_config']
+        trainer.fit(
+            model = wrapper_model,
+            train_dataloaders = datamodule,
+            **fit_config
+        )
+        # ---plot metric curves
+        if trainer_config['logger']:
+            plot_metrics(trainer.log_dir)
+
+        # testing phase
+        trainer.test(
+            model = wrapper_model,
+            dataloaders = datamodule
+        )
+
+        # save the last checkpoint manually if set enable_checkpointing = False
+        default_root_dir = trainer_config['default_root_dir']
+        checkpoint_dir = os.path.join(default_root_dir, 'checkpoints')
+        os.makedirs(checkpoint_dir, exist_ok = True)
+        epoch = trainer.current_epoch-1
+        step = trainer.global_step//2
+        checkpoint_name = f'epoch={epoch}-step={step}.ckpt'
+        checkpoint_path = os.path.join(checkpoint_dir, checkpoint_name)
+        trainer.save_checkpoint(checkpoint_path)
+
+        self.test_on_testdataset(
+            vqgan = wrapper_model.model,
+            trainer = trainer  
+        )
+
+    def test_on_testdataset(self, vqgan, trainer):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        log_dir = trainer.log_dir
+
+        images, labels = next(iter(trainer.test_dataloaders))
+        images = images.to(device)
+        labels = labels.to(device)
+        vqgan.to(device)
+
+        n_samples = min(len(images), 2)
+        org_images = images[0:n_samples]
+        rec_images = vqgan.reconstruct(org_images)
+
+        org_images = (org_images+1)/2
+        # save images
+        dir_name = log_dir + '/images'
+        os.makedirs(dir_name, exist_ok = True)
+        file_name = f'test_on_epoch_{trainer.max_epochs-1}.png'
+        path = os.path.join(
+            dir_name,
+            file_name
+        )
+        save_image(
+            images = [org_images.cpu(), rec_images.cpu()],
+            path = path,
+            titles = ['original', 'reconstructed']
+        )
+
+
+class TrainingTransformer:
+    def __init__(self, config_file):
+        with open(config_file, 'r') as file:
+            configs = yaml.safe_load(file)
+        self.configs = configs
+    def run(self, max_epochs: int = None, ckpt_path: str = None, limit_batches: bool = False):
+        vqgan_checkpoint = self.configs['vqgan_checkpoint']
+        Transformer_config = self.configs['Transformer_config']
+        dataset_config = self.configs['dataset_config']
+        training_config = self.configs['training_config']
+        if max_epochs:
+            training_config['trainer_config']['max_epochs'] = max_epochs
+        if ckpt_path:
+            training_config['fit_config']['ckpt_path'] = ckpt_path
+
+        # Transformer model
+        transfomer_model = Transformer_config
+        
+        # dataset
+        datamodule = datasets.LitImageGenerationDataset(**dataset_config)
+
+        # Wrapper model
+        optimizer_config = training_config['optimizer_config']
+        wrapper_model = Transformer_Wrapper(
+            model = transfomer_model,
+            vqgan_checkpoint = vqgan_checkpoint,
+            **optimizer_config
+        )
+
+        # trainer config
+        trainer_config = training_config['trainer_config']
+        # ---logger
+        if trainer_config['logger']:
+            trainer_config['logger'] = L.pytorch.loggers.CSVLogger(
+                save_dir = trainer_config['default_root_dir'],
+                name = 'logs'
+            )
+
+        # for reproducibility: seed_everything, deterministic
+        L.seed_everything(42, workers=True)
+        if limit_batches: # for testing purpose
+            limit_batches = {
+                'limit_train_batches': 2,
+                'limit_test_batches': 2,
+                'limit_val_batches': 2
+            }
+        else:
+            limit_batches = {}
+        lr_monitor = L.pytorch.callbacks.LearningRateMonitor(
+            logging_interval = 'step'
+        )
+        trainer = L.Trainer(
+            **trainer_config,
+            deterministic=True,
+            enable_checkpointing = False, 
+            callbacks = [lr_monitor],
+            **limit_batches
+        )
+
+        # training phase
+        fit_config = training_config['fit_config']
+        trainer.fit(
+            model = wrapper_model,
+            train_dataloaders = datamodule,
+            **fit_config
+        )
+        # ---plot metric curves
+        if trainer_config['logger']:
+            plot_metrics(trainer.log_dir)
+
+        # testing phase
+        trainer.test(
+            model = wrapper_model,
+            dataloaders = datamodule
+        )
+
+        # save the last checkpoint manually if set enable_checkpointing = False
+        default_root_dir = trainer_config['default_root_dir']
+        checkpoint_dir = os.path.join(default_root_dir, 'checkpoints')
+        os.makedirs(checkpoint_dir, exist_ok = True)
+        checkpoint_name = f'epoch={trainer.current_epoch-1}-step={trainer.global_step}.ckpt'
+        checkpoint_path = os.path.join(checkpoint_dir, checkpoint_name)
+        trainer.save_checkpoint(checkpoint_path)
+
+        self.test_on_testdataset(
+            transformer_wrapper = wrapper_model,
+            trainer = trainer  
+        )
+
+    def test_on_testdataset(self, transformer_wrapper, trainer):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        log_dir = trainer.log_dir
+
+        transformer_wrapper.to(device)
+        image, label = next(iter(trainer.test_dataloaders))
+        
+        n_samples = 4
+        idx = torch.randperm(len(label))[:n_samples]
+        real_image = image[idx]
+        real_image = (real_image+1.0)/2.0 # [-1,1] -> [0,1]
+        label = label[idx]
+        generated_image_token = transformer_wrapper.model.generate_image_token(
+            n_samples  = n_samples, 
+            label = label,
+            n_step = 12,
+            temperature = 1.0,
+            masking_method = 'cosine'
+        )
+        gen_image = transformer_wrapper.vqgan_pretrained.indices_to_image(generated_image_token)
+        # save images
+        dir_name = log_dir + '/images'
+        os.makedirs(dir_name, exist_ok = True)
+        file_name = f'test_on_epoch_{trainer.max_epochs-1}.png'
+        path = os.path.join(
+            dir_name,
+            file_name
+        )
+        save_image(
+            images = [real_image.cpu(),gen_image.cpu()],
+            path = path,
+            titles = ['real image','generated']
+        )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--phase', type = str)
+    parser.add_argument('--config_file', type = str)
+    parser.add_argument('--max_epochs', type = int, default = None)
+    parser.add_argument('--ckpt_path', type = str, default = None)
+    parser.add_argument('--limit_batches', type = bool, default = False)
+    
+    args = parser.parse_args()
+    assert args.phase in ['vqgan', 'transformer']
+    if args.phase == 'vqgan':
+        training = TrainingVQGAN(args.config_file)
+    else:
+        training = TrainingTransformer(args.config_file)
+    training.run(
+        max_epochs = args.max_epochs,
+        ckpt_path = args.ckpt_path,
+        limit_batches = args.limit_batches
+    )
